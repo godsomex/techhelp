2f93aae9771e42036e31b0f9fd6a1abc96513758
Olaoluwadunni <oluwatimileyinakinpelu49@gmail.com>
2021-06-21 20:03:15 +0100

    Merge pull request #2 from godsomex/feature/navbar
    Navbar added

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <header>
+      <nav>
+        <div className="container">
+          <span className="logo">Techzone</span>
+
+          <ul className="navbar">
+            <li>
+              <a href="homepage.html">Home</a>
+            </li>
+            <li>
+              <a href="#">About Us</a>
+            </li>
+            <li>
+              <a href="#">Profile</a>
+            </li>
+            <li>
+              <a href="#">Contact Us</a>
+            </li>
+            <li>
+              <a href="signin.html">Sign In</a>
+            </li>
+            <li>
+              <a href="signup.html">Sign Up</a>
+            </li>
+            <input type="text" placeholder="Search.." />
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
