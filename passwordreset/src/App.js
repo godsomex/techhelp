ef3bb9b486c4bf53cd33196c911d18418de66d2d
Maduako Akachi <maduakoakachi.com>
2021-06-26 14:08:31 +0100

    password reset component

---
+++ passwordreset/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='logo'></div>
+      <h1 className='title'>TechZone</h1>
+      <h1 className='login'>Password Reset</h1>
+      <input className='text' type="text" placeholder="email" name="" />
+      <input className='btn' type="button" name="" value="Reset" />
+    </div>
+  );
+};
+
+export default App;
