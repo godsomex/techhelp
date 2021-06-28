2b480129b8f8cf6ed29a1210e7bd452d0a21c485
AdeboyeJuliet <julietadeboye01@gmail.com>
2021-06-28 21:35:58 +0100

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import Navbar from './components/Navbar';
-<<<<<<< HEAD
 import Searchpage from './components/Searchpage';
-=======
 import About from './components/About';
 import Listing from './components/Listing';
->>>>>>> main
 
 function App() {
   return (
     <>
     <div>
       <Navbar />
-<<<<<<< HEAD
       
       <Searchpage />
-=======
       <About />
->>>>>>> main
     </div>
 
     <div>
